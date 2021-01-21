125508f52164a4ef8103ad0a669b30bfb73ccf8b
Arttu Mykkänen <arttu.mykkanen@outlook.com>
2021-01-21 17:34:39 +0200

    Add admin field to user, try to be consistent with string formatting style

--- app/models.py
+++ app/models.py
@@ -12,9 +12,10 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(128), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    admin = db.Column(db.Boolean())
 
     def __repr__(self):
-        return 'User {}'.format(self.username)
+        return f'User {self.username}'
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
