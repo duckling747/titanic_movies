e60afd618c9c388a90218aac6ac898c5a8a789c4
Arttu Mykkänen <arttu.mykkanen@outlook.com>
2021-02-09 21:27:14 +0200

    enable csrf protection & csp

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_talisman import Talisman
+from flask_wtf import CSRFProtect
 
 from .config import Config
 
@@ -10,6 +12,19 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+csrf = CSRFProtect(app)
+csp = {
+    'object-src': '\'none\'',
+    'default-src': '\'self\' data:',
+    'font-src': '\'self\'',
+    'script-src': '\'unsafe-inline\' https: \'self\'',
+    'base-uri': '\'none\'',
+}
+talisman = Talisman(
+    app,
+    content_security_policy=csp,
+    content_security_policy_nonce_in=['script-src'],
+)
 
 login = LoginManager(app)
 login.login_view = 'login'
