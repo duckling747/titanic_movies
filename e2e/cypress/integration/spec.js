b0f69cb5619532b8aaa47dc4e093aff655e0cab1
Arttu Mykkänen <arttu.mykkanen@outlook.com>
2021-02-19 08:26:11 +0200

    Use cypress for e2e testing

---
+++ e2e/cypress/integration/spec.js
@@ -0,0 +1,5 @@
+describe("index page", () => {
+    it("loads without error", () => {
+        cy.visit("http://localhost:80");
+    })
+})
---
+++ test_e2e.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+exec docker run -it -v $(pwd)/e2e:/e2e --workdir /e2e\
+ -e CYPRESS_VIDEO=false\
+ -e CYPRESS_baseUrl=http://localhost:80\
+ --network="host"\
+ cypress/included:3.4.0
