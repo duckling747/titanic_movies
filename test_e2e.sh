2a1a040190b1689746a13143fb9d7fdcc3c922b3
Arttu Mykkänen <arttu.mykkanen@outlook.com>
2021-02-19 11:38:20 +0200

    use latest cypress docker image

--- test_e2e.sh
+++ test_e2e.sh
@@ -3,4 +3,4 @@ exec docker run -it -v $(pwd)/e2e:/e2e --workdir /e2e\
  -e CYPRESS_VIDEO=false\
  -e CYPRESS_baseUrl=http://localhost:80\
  --network="host"\
- cypress/included:3.4.0
+ cypress/included:6.5.0
